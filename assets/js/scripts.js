ffe270a323679bda906649c88a57c0703d1a6f05
Dergi Samayoa <dergio@gmail.com>
2023-11-04 01:21:42 -0600

    Home y footer finalizados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,11 @@
+function getYear() {
+    const dataYear = document.getElementById("copy");
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    dataYear.textContent = year;
+}
+
+
 console.dir(window);
 
 console.dir(document)
